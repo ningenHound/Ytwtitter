128bf38a0b401c317a9e308e050d4fa310068fdf
Francisco Galiano <frangv87@gmail.com>
2023-12-12 18:20:07 -0300

    link logout

--- resources/views/parts/sidenav.blade.php
+++ resources/views/parts/sidenav.blade.php
@@ -1,5 +1,5 @@
 <ul id="slide-out" class="sidenav sidenav-fixed">
     <li><a href="#!">Inicio</a></li>
     <li><a href="#!">Mi Perfil</a></li>
-    <li><a href="#!">Cerrar Sesión</a></li>
+    <li><a href="/logout">Cerrar Sesión</a></li>
 </ul>
